5caf209352aa7a191aab898ceb6b26d9147bdca8
Minh-Quan Nguyen <minhquanphungnguyen@yahoo.com>
2015-05-21 21:28:04 -0700

    adding files

---
+++ CMakeFiles/videoCap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/videoCap.dir/videoCap.cpp.o"
+  "videoCap.pdb"
+  "videoCap"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/videoCap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
